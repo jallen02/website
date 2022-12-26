d8a3bfea651a61d6388f6ceb3c01fed877d923ae
Jason Allen <j.nella12@gmail.com>
2022-12-26 02:13:45 -0500

    add yew and add first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+        { "Welcome to my website! This site is a work in progress so bear with any inconsistencies. This site is built completely using rust using the code located at https://github.com/jallen02/website" }
+        </div>
+    }
 }
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
+
