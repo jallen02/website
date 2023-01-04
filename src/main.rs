775ac477b0895d4371f3917c50b94a681962aaef
Jason Allen <j.nella12@gmail.com>
2023-01-03 23:58:38 -0500

    switch to yew

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
-#[cfg(target_arch = "wasm32")]
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <div class="app">
+            <img class="profile_pic" src="assets/profile_pic.jpeg"/>
+            <Title />
+            <Footer />
+        </div>
+    }
+}
+
+#[function_component(Title)]
+fn title() -> Html {
+    html! {
+        <div class="heading">
+            <h1 class="title">{"Jason Allen's website"}</h1>
+            <p class="explainer">
+                {"I am a backend engineer trying his hand at web dev."}
+            </p>
+            <p class="explainer">
+                {"Code for this website is available on "}
+                <a href="https://github.com/jallen02/website"> {"Github"} </a>
+            </p>
+        </div>
+    }
+}
+
+#[function_component(Footer)]
+fn footer() -> Html {
+    html! {
+        <footer>
+            <p>
+                {"This website is written 100% from scratch in Rust (as well as CSS for styling) and utilizes the Yew framework. "}
+                {"This website is a work in progress, and will have visual inconsistencies as I learn and iterate."}
+            </p>
+        </footer>
+    }
+}
+
 fn main() {
-    // Make sure panics are logged in the browser's console.error
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log
-    tracing_wasm::set_as_global_default();
-
-    let options = eframe::WebOptions::default();
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::start_web(
-            "canvas_id",
-            options,
-            Box::new(|cc| Box::new(website::App::new(cc))),
-        )
-        .await
-        .expect("failed to start eframe");
-    })
+    yew::Renderer::<App>::new().render();
 }
