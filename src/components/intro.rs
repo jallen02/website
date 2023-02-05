a596eb90c6afbc86c4c04c3d994b8fb7f347c0dc
Jason Allen <j.nella12@gmail.com>
2023-02-05 14:01:33 -0500

    integrate with tailwind

--- src/components/intro.rs
+++ src/components/intro.rs
@@ -3,11 +3,11 @@ use leptos::{component, view, IntoView, Scope};
 #[component]
 pub fn Intro(cx: Scope) -> impl IntoView {
     view! { cx,
-        <div class="heading">
-            <img class="heading-item profile-pic" src="assets/profile_pic.jpeg"/>
-            <p class="heading heading-item">
-                <h1> "Jason Allen" </h1>
-                <span> "Backend software dev." </span>
+        <div class="flex p-1 items-center">
+            <img class="rounded-full w-32 h-32" src="assets/profile_pic.jpeg"/>
+            <p class="align-super p-3">
+                <h1 class="text-3xl"> "Jason Allen" </h1>
+                <span> "Backend software dev" </span>
             </p>
         </div>
     }
