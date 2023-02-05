32c265062b0f3642f76ff831d33d509271a74b02
Jason Allen <j.nella12@gmail.com>
2023-02-04 20:00:31 -0500

    switch to leptos

---
+++ src/components/intro.rs
@@ -0,0 +1,14 @@
+use leptos::{component, view, IntoView, Scope};
+
+#[component]
+pub fn Intro(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <div class="heading">
+            <img class="heading-item profilepic" src="assets/profile_pic.jpeg"/>
+            <p class="heading heading-item">
+                <h1> "Jason Allen" </h1>
+                <span> "Backend software dev." </span>
+            </p>
+        </div>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,15 @@
-use yew::prelude::*;
+use crate::components::intro::{Intro, IntroProps};
+use leptos::{mount_to_body, view};
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <div class="app">
-            <img class="profile_pic" src="assets/profile_pic.jpeg"/>
-            <Title />
-            <Footer />
-        </div>
-    }
-}
-
-#[function_component(Title)]
-fn title() -> Html {
-    html! {
-        <div class="heading">
-            <h1 class="title">{"Jason Allen's website"}</h1>
-            <p class="explainer">
-                {"I am a backend engineer trying his hand at web dev."}
-            </p>
-            <p class="explainer">
-                {"Code for this website is available on "}
-                <a href="https://github.com/jallen02/website"> {"Github"} </a>
-            </p>
-        </div>
-    }
-}
-
-#[function_component(Footer)]
-fn footer() -> Html {
-    html! {
-        <footer>
-            <p>
-                {"This website is written 100% from scratch in Rust (as well as CSS for styling) and utilizes the Yew framework. "}
-                {"This website is a work in progress, and will have visual inconsistencies as I learn and iterate."}
-            </p>
-        </footer>
-    }
-}
+mod components;
+mod lib;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|cx| {
+        view! { cx,
+            <Intro/>
+        }
+    })
 }
