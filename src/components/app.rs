a0dd4aadf9fc7cce703645d36a79528dd487dd5b
Jason Allen <j.nella12@gmail.com>
2023-02-08 22:19:21 -0500

    start work on posts hosting

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,9 +1,10 @@
-use crate::components::intro::{Intro, IntroProps};
+use crate::components::{intro::{Intro, IntroProps}, posts::{Posts, PostsProps}};
 use leptos::{component, view, IntoView, Scope};
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     view! { cx,
         <Intro/>
+        <Posts/>
     }
 }
